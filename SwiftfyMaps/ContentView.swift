cb6575e5dac523275212299239101c33ee13c850
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-01-19 17:11:56 +0200

    Location Model and files added

--- SwiftfyMaps/ContentView.swift
+++ SwiftfyMaps/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  SwiftfyMaps
-//
-//  Created by Pat on 2023/01/19.
-//
-
 import SwiftUI
 
 struct ContentView: View {
---
+++ SwiftfyMaps/Models/Location.swift
@@ -0,0 +1,11 @@
+import Foundation
+import CoreLocation
+
+struct Location{
+    var name: String
+    var cityName : String
+    var coordinates: CLLocationCoordinate2D
+    var description: String
+    var imageNames: [String]
+    var link: String
+}
