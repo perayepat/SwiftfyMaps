c94cf3e42e7dcd4f1147be7fa4a36c97719b0b25
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-01-19 14:26:27 +0200

    Initial Commit

---
+++ SwiftfyMaps/SwiftfyMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfyMapsApp.swift
+//  SwiftfyMaps
+//
+//  Created by Pat on 2023/01/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfyMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
